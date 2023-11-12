4d7f5212a4341910fa03759930c5c77db3a8b4dc
obsidian0215 <luyifan20@mails.ucas.edu.cn>
2023-11-12 10:06:30 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,8 +20,8 @@ if [ "$(uname)" == "Darwin" ]; then
     echo "cp --parents -u -r .vim* ~/"
     cp --parents -u -r .vim* /Users/${USER}/workspace/github/tmp
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    echo "cp --parents -u tmux.conf.linux ~/.tmux.conf"
-    cp --parents -u tmux.conf.linux ~/.tmux.conf
+    echo "cp -u tmux.conf.linux ~/.tmux.conf"
+    cp -u tmux.conf.linux ~/.tmux.conf
     echo "cp --parents -u fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish"
     cp --parents -u fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
     echo "cp -r --parents -u .vim* ~/"
